996eba5e747e0f8a70213a24fd48eeff68485c35
Alan Christie <alan.christie@matildapeak.com>
2024-07-31 16:29:36 +0100

    refactor: Style tweak

--- checker.py
+++ checker.py
@@ -13,15 +13,18 @@ from decoder import decoder
 
 # The repository we're run in is mounted into the container under /github/workspace,
 # which is the image WORKDIR value - so '.' is the root of the repository to check.
+# When run as a pre-commit hook, the repository is mounted under /src
+# but the pre-commit logic also sets WORKDIR accordingly, so "." is still the root.
 GITHUB_WORKSPACE: str = "."
+# The Data Manager directory (manifests)
 DM_DIR: str = f"{GITHUB_WORKSPACE}/data-manager"
+# FIle globs for manifests and Nextflow files
 MANIFEST_FILE_GLOB: str = f"{DM_DIR}/manifest-*.yaml"
 NEXTFLOW_FILE_GLOB: str = f"{GITHUB_WORKSPACE}/**/*.nf"
 
-# Production tags are 2 or 3-number version strings
-# i.e. 2024.3 or 1.0.2
+# Production tags are 2 or 3-number version strings, i.e. 2024.3 or 1.0.2
 VALID_IMAGE_TAG_PATTERN: re.Pattern = re.compile(r"^[0-9]+\.[0-9]+(\.[0-9]+)?$")
-# What does a nextflow container definition look like?
+# What does a nextflow container declaration look like?
 #   container 'informaticsmatters/vs-moldb:latest'
 NEXTFLOW_CONTAINER_PATTERN: re.Pattern = re.compile(r"^\s+container '((?P<iname>\S+)(:(?P<itag>\S+)))?'\s*$")
 
@@ -31,13 +34,14 @@ ERRORS: List[str] = []
 
 def error(msg: str) -> None:
     """Prints and collects an error message"""
-    ERRORS.append(msg)
     print(f"ERROR: {msg}")
+    ERRORS.append(msg)
 
 
 def check() -> None:
     """Runs the built-in tag checks for job-definition files
     and nextflow files."""
+    # Look for Job manifests
     jd_files: List[str] = []
     manifest_files: List[str] = glob.glob(MANIFEST_FILE_GLOB)
     for manifest_file in manifest_files:
@@ -47,6 +51,7 @@ def check() -> None:
                 print(f"Missing job-definition-files in {manifest_file}")
                 sys.exit(1)
             jd_files += manifest["job-definition-files"]
+    # Process Job definition files (declared in manifests)
     if not jd_files:
         error("No job definition files found")
     for jd_file in jd_files:
@@ -61,6 +66,7 @@ def check() -> None:
                     msg: str = f"Invalid image tag '{image_tag}' for job '{jd_name}' in {jd_path}"
                     error(msg)
 
+    # Process Nextflow files
     nf_files: List[str] = glob.glob(NEXTFLOW_FILE_GLOB, recursive=True)
     for nf_file in nf_files:
         with open(nf_file, encoding="utf-8") as file:
@@ -73,11 +79,13 @@ def check() -> None:
                     else:
                         error(f"Missing container tag in {nf_file}:{line_no}")
 
-check()
-if ERRORS:
-    print("------")
-    if len(ERRORS) == 1:
-        print("FAILED - there is 1 invalid tag")
-    else:
-        print(f"FAILED - there are {len(ERRORS)} invalid tags")
-    sys.exit(1)
+if __name__ == "__main__":
+
+    check()
+    if ERRORS:
+        print("------")
+        if len(ERRORS) == 1:
+            print("FAILED - there is 1 invalid tag")
+        else:
+            print(f"FAILED - there are {len(ERRORS)} invalid tags")
+        sys.exit(1)
