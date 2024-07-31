eb26d3be6d8452cf834fecaeba9de716d7b8bc59
Alan Christie <alan.christie@matildapeak.com>
2024-07-31 12:32:39 +0100

    feat: Quieter

--- checker.py
+++ checker.py
@@ -32,11 +32,11 @@ ERRORS: List[str] = []
 def error(msg: str) -> None:
     """Prints and collects an error message"""
     ERRORS.append(msg)
-    print(f" ERROR: {msg}")
+    print(f"ERROR: {msg}")
 
 
 def check() -> None:
-    """Runs the built-in tag checks for manifest files
+    """Runs the built-in tag checks for job-definition files
     and nextflow files."""
     jd_files: List[str] = []
     manifest_files: List[str] = glob.glob(MANIFEST_FILE_GLOB)
@@ -47,46 +47,35 @@ def check() -> None:
                 print(f"Missing job-definition-files in {manifest_file}")
                 sys.exit(1)
             jd_files += manifest["job-definition-files"]
-
     if not jd_files:
-        error("No job definition files found in manifest files")
-
+        error("No job definition files found")
     for jd_file in jd_files:
-        print(f"Checking Job Manifest '{jd_file}'...")
         jd_path: str = os.path.join(DM_DIR, jd_file)
         with open(jd_path, encoding="utf-8") as f_jd:
             jd = yaml.safe_load(f_jd)
             jd_munch: DefaultMunch = DefaultMunch.fromDict(jd)
             for jd_name in jd_munch.jobs:
                 _, image_tag = decoder.get_image(jd_munch.jobs[jd_name])
-                tag_match: re.Match = VALID_IMAGE_TAG_PATTERN.match(image_tag)
+                tag_match = VALID_IMAGE_TAG_PATTERN.match(image_tag)
                 if not tag_match:
-                    msg: str = f"Invalid image tag '{image_tag}' for job '{jd_name}'"
+                    msg: str = f"Invalid image tag '{image_tag}' for job '{jd_name}' in {jd_path}"
                     error(msg)
 
     nf_files: List[str] = glob.glob(NEXTFLOW_FILE_GLOB, recursive=True)
-
-    if not nf_files:
-        print("No Nextflow files found")
-
     for nf_file in nf_files:
         short_path: str = nf_file.replace(GITHUB_WORKSPACE, "")
-        print(f"Checking Nextflow '{short_path}'...")
         with open(nf_file, encoding="utf-8") as file:
             for line_no, line in enumerate(file, start=1):
                 if container_match := NEXTFLOW_CONTAINER_PATTERN.match(line):
                     if itag := container_match.group("itag"):
                         tag_match: re.Match = VALID_IMAGE_TAG_PATTERN.match(itag)
                         if not tag_match:
-                            error(f"Invalid image tag '{itag}' on line {line_no} in {short_path}")
+                            error(f"Invalid container tag '{itag}' on line {line_no} of {short_path}")
                     else:
-                        error(f"Missing image tag in container definition on line {line_no} in {short_path}")
+                        error(f"Missing container tag on line {line_no} of {short_path}")
 
 check()
-if not ERRORS:
-    print("--")
-    print("OK")
-else:
+if ERRORS:
     print("------")
     if len(ERRORS) == 1:
         print("FAILED - there is 1 invalid tag")
