6e1a1acfa657cbd97c1d0e54600ddcae9885fae2
Alan Christie <alan.christie@matildapeak.com>
2024-07-31 07:19:53 +0100

    feat: Tweak checker logic (errors)

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # Squonk2 job definition container tag checker action
 A GitHub Action that checks the repository's container image tags
 present in Squonk2 Job Definition files. All container images must have
-explicit tags, i.e. cannot be `latest` or `stable`.
+explicit and official tags, e.g. must be a sequence of numbers
+using the pattern "N.N[.N]".
 
 ## Example usage
-The repository is automaticlaly mapped into the action container,
-so you simply have to identify the action, after having checked out the
-repository..
+The repository under test is automatically mapped into the action container,
+so you simply have to identify the action, after checking out the
+repository's files...
 
 ```yaml
 - name: Checkout
--- checker.py
+++ checker.py
@@ -14,20 +14,28 @@ from decoder import decoder
 # The repository to check is mounted into the container under /github/workspace.
 
 GITHUB_WORKSPACE: str = "/Users/alan/Code/InformaticsMatters/virtual-screening"
+#GITHUB_WORKSPACE: str = "/github/workspace"
 DM_DIR: str = f"{GITHUB_WORKSPACE}/data-manager"
-MANIFEST_FILES: str = f"{DM_DIR}/manifest-*.yaml"
-
-# Iterate through all the Data Manager manifests,
-# collecting the names of the the Job definition files.
+MANIFEST_FILE_GLOB: str = f"{DM_DIR}/manifest-*.yaml"
 
 # Production tags are 2 or 3-number version strings
 # i.e. 2024.3 or 1.0.2
-VALID_IMAGE_TAG_PATTERN: re.Pattern = re.compile("^[0-9]+\.[0-9]+(\.[0-9]+)?$")
+VALID_IMAGE_TAG_PATTERN: re.Pattern = re.compile(r"^[0-9]+\.[0-9]+(\.[0-9]+)?$")
+
+# Collected errors
+ERRORS: List[str] = []
+
+
+def error(msg: str) -> None:
+    """Prints and collects an error message"""
+    ERRORS.append(msg)
+    print(f" ERROR: {msg}")
+
 
 def check() -> None:
     """Runs the check."""
     jd_files: List[str] = []
-    manifest_files: List[str] = glob.glob(MANIFEST_FILES)
+    manifest_files: List[str] = glob.glob(MANIFEST_FILE_GLOB)
     for manifest_file in manifest_files:
         with open(manifest_file, "r", encoding="utf-8") as f_manifest:
             manifest = yaml.safe_load(f_manifest)
@@ -36,10 +44,11 @@ def check() -> None:
                 sys.exit(1)
             jd_files += manifest["job-definition-files"]
 
-    print(f"Checking {len(jd_files)} job definition files...")
+    if not jd_files:
+        error("No job definition files found in manifest files")
 
     for jd_file in jd_files:
-        print(f"Checking {jd_file}...")
+        print(f"Checking Job Manifest '{jd_file}'...")
         jd_path: str = os.path.join(DM_DIR, jd_file)
         with open(jd_path, "r", encoding="utf-8") as f_jd:
             jd = yaml.safe_load(f_jd)
@@ -48,7 +57,16 @@ def check() -> None:
                 _, image_tag = decoder.get_image(jd_munch.jobs[jd_name])
                 tag_match: re.Match = VALID_IMAGE_TAG_PATTERN.match(image_tag)
                 if not tag_match:
-                    print(f"ERROR: Invalid image tag '{image_tag}' for job '{jd_name}'")
-                    sys.exit(1)
+                    msg: str = f"Invalid image tag '{image_tag}' for job '{jd_name}'"
+                    error(msg)
+
+
 
 check()
+if not ERRORS:
+    print("--")
+    print("OK")
+else:
+    print("------")
+    print("FAILED - there were invalid tags")
+    sys.exit(1)
